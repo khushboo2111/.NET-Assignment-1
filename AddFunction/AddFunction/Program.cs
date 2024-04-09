79a2dc4a2d1cc7c4c907a0e0f1a6e4d02156b6dc
khushboo2111 <162410725+khushboo2111@users.noreply.github.com>
2024-04-09 20:03:31 +0530

    Added code

---
+++ AddFunction/AddFunction/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Add_Function
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+
+            Console.WriteLine("Enter value of 'a':");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter value of 'b':");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            c = Add(a, b);
+            Console.WriteLine("a + b = {0}", c);
+        }
+
+        static public int Add(int x, int y)
+        {
+            int result = x + y;
+            return result;
+        }
+    }
+}
+
